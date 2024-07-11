233f86a8d6797d4bc6bebe5829db0b81e667b633
NatanaelLimaDEV <natanaellimaofc@gmail.com>
2024-07-11 14:44:09 -0300

    menu

--- funcoes.js
+++ funcoes.js
@@ -11,3 +11,11 @@ function clickMenu(){
         iconMenu.innerHTML = 'close'
     }
 }
+
+function telaTamanho(){
+    if(window.innerWidth >= 768){
+        menu.style.display = 'block'
+    } else{
+        menu.style.display = 'none'
+    }
+}
